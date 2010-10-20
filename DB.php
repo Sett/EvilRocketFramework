fe63e6cdb29b38a24ff3e191fbbc401e4d3df918
breathless <breathless@513b48f0-a4ba-4d1f-9748-a1db15791537>
2010-10-20 09:04:10 +0000

    git-svn-id: https://svn.ruimperium.com:444/repos/Common@2071 513b48f0-a4ba-4d1f-9748-a1db15791537

---
+++ DB.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Evil DB Front Controller Plugin 
+ */
+ 
+    class Evil_DB extends Zend_Controller_Plugin_Abstract
+    {
+        public function routeStartup(Zend_Controller_Request_Abstract $request)
+        {
+            parent::routeStartup($request);
+
+            $this->controllerDrivenDB($request);
+        }
+
+        public function controllerDrivenDB($request)
+        {
+            $controller = $request->getControllerName();
+            $config = Zend_Registry::get('config');
+
+            if (isset($config['resources']['db'][$controller]))
+            {
+                $db = Zend_Db::factory(
+                    $config['resources']['db'][$controller]['adapter'],
+                    $config['resources']['db'][$controller]['params']);
+
+                Zend_Registry::set('db-prefix',$config['resources']['db'][$controller]['prefix']);
+            }
+            else
+            {
+                $db = Zend_Db::factory(
+                    $config['resources']['db']['adapter'],
+                    $config['resources']['db']['params']);
+                Zend_Registry::set('db-prefix',$config['resources']['db']['prefix']);
+            }
+
+            Zend_Registry::set('db',$db);
+            Zend_Db_Table_Abstract::setDefaultAdapter($db);
+        }
+
+        public function fallbackDB ()
+        {
+            // TODO: Fallback support
+        }
+
+        public function mirrorDB ()
+        {
+            // TODO: Mirroring support
+        }
+
+        public function shardDB ()
+        {
+            // TODO: Sharding support
+        }
+    }
