bf9e5e11a03672b1be47b315e5c9fb942ae1b55c
breathless <breathless@ruimperium.com>
2010-10-26 16:05:45 +0000

    git-svn-id: https://svn.ruimperium.com:444/repos/Common/Evil@2499 513b48f0-a4ba-4d1f-9748-a1db15791537

--- Composite/H2D.php
+++ Composite/H2D.php
@@ -1,14 +1,72 @@
-<?php
+    <?php
 
     class Evil_Composite_H2D implements Evil_Composite_Interface,
         ArrayAccess, Countable, Iterator
     {
-       private $_items = array();
-       private $_type;
+        public $_items = array();
+        private $_type;
 
-       private $_fixed;
+        private $_fixed;
 
-       private $_fluid;
+
+        public function count ()
+        {
+           // TODO: Implement count() method
+        }
+
+        /**
+         * (PHP 5 &gt;= 5.1.0)<br/>
+         * Return the current element
+         * @link http://php.net/manual/en/iterator.current.php
+         * @return mixed Can return any type.
+         */
+        public function current () {
+            // TODO: Implement current() method.
+        }
+
+        /**
+         * (PHP 5 &gt;= 5.1.0)<br/>
+         * Return the key of the current element
+         * @link http://php.net/manual/en/iterator.key.php
+         * @return scalar scalar on success, integer
+         * 0 on failure.
+         */
+        public function key () {
+            // TODO: Implement key() method.
+        }
+
+        /**
+         * (PHP 5 &gt;= 5.1.0)<br/>
+         * Move forward to next element
+         * @link http://php.net/manual/en/iterator.next.php
+         * @return void Any returned value is ignored.
+         */
+        public function next () {
+            // TODO: Implement next() method.
+        }
+
+        /**
+         * (PHP 5 &gt;= 5.1.0)<br/>
+         * Rewind the Iterator to the first element
+         * @link http://php.net/manual/en/iterator.rewind.php
+         * @return void Any returned value is ignored.
+         */
+        public function rewind () {
+            // TODO: Implement rewind() method.
+        }
+
+        /**
+         * (PHP 5 &gt;= 5.1.0)<br/>
+         * Checks if current position is valid
+         * @link http://php.net/manual/en/iterator.valid.php
+         * @return boolean The return value will be casted to boolean and then evaluated.
+         * Returns true on success or false on failure.
+         */
+        public function valid () {
+            // TODO: Implement valid() method.
+        }
+
+        private $_fluid;
 
         /**
          * (PHP 5 &gt;= 5.1.0)<br/>
@@ -64,146 +122,136 @@
          * </p>
          * @return void
          */
-        public function offsetUnset ($offset) {
+        public function offsetUnset ($offset)
+        {
             // TODO: Implement offsetUnset() method.
         }
 
-       public function __construct ($type)
-       {
-           $this->_type = $type;
+        public function __construct ($type)
+        {
+            $this->_type = $type;
 
-           $prefix = Zend_Registry::get('db-prefix');
+            $prefix = Zend_Registry::get ('db-prefix');
 
-           $this->_fixed = new Zend_Db_Table($prefix.$type.'s-fixed');
-           $this->_fluid = new Zend_Db_Table($prefix.$type.'s-fluid'); // ?
+            $this->_fixed = new Zend_Db_Table($prefix . $type . 's-fixed');
+            $this->_fluid = new Zend_Db_Table($prefix . $type . 's-fluid'); // ?
 
-           $info = $this->_fixed->info();
-           $this->_fixedschema = $info['cols'];
-       }
+            $info = $this->_fixed->info ();
+            $this->_fixedschema = $info['cols'];
+        }
 
-       public function where ($key, $selector, $value)
-       {
-            $mode  = mb_substr($selector,0,1);
-            $query = mb_substr($selector,1);
-
-            switch ($mode)
+        public function where ($key, $selector, $value)
+        {
+            switch ($selector)
             {
                 case '=':
-                    list($key, $value) = explode('=', $query);
-
-                    if (in_array($key, $this->_fixedschema))
-                    {
-                        $rows = $this->_fixed->fetchAll(
+                    if (in_array ($key, $this->_fixedschema)) {
+                        $rows = $this->_fixed->fetchAll (
                             $this->_fixed
-                                ->select()
-                                ->from(
-                                    $this->_fixed,
-                                        array($this->_type.'_id')
-                                       )
-                                ->where($key.' = ?', $value));
+                                ->select ()
+                                ->from (
+                                $this->_fixed,
+                                array($this->_type . '_id')
+                            )
+                                ->where ($key . ' = ?', $value));
 
-                        $ids = $rows->toArray();
+                        $ids = $rows->toArray ();
 
-                                        foreach ($ids as $id)
-                                        {
-                                                $id = $id[$this->_type.'_id'];
-                                                $this->_items[$id] = new H2D_Object($this->_type, $id);
-                                        }
+                        foreach ($ids as $id)
+                        {
+                            $id = $id[$this->_type . '_id'];
+                            $this->_items[$id] = new Evil_Object_H2D($this->_type, $id);
+                        }
                     }
                     else
                     {
-                        $rows = $this->_fluid->fetchAll(
+                        $rows = $this->_fluid->fetchAll (
                             $this->_fluid
-                                ->select()
-                                ->from(
-                                    $this->_fluid,
-                                        array('i')
-                                       )
-                                ->where('k = ?', $key)
-                                ->where('v = ?', $value));
+                                ->select ()
+                                ->from (
+                                $this->_fluid,
+                                array('i')
+                            )
+                                ->where ('k = ?', $key)
+                                ->where ('v = ?', $value));
 
-                        $ids = $rows->toArray();
+                        $ids = $rows->toArray ();
 
-                                        foreach ($ids as $id)
-                                        {
-                                                $id = $id['i'];
-                                                $this->_items[$id] = new H2D_Object($this->_type, $id);
-                                        }
+                        foreach ($ids as $id)
+                        {
+                            $id = $id['i'];
+                            $this->_items[$id] = new Evil_Object_H2D($this->_type, $id);
+                        }
                     }
 
-                break;
+                    break;
 
-                        case ':':
-                    list($key, $values) = explode(':', $query);
+                case ':':
+                    $values = explode (',', $values);
 
-                                $values = explode(',',$values);
+                    foreach ($values as &$value)
+                    $value = '"' . $value . '"';
 
-                                foreach ($values as &$value)
-                                    $value = '"'.$value.'"';
+                    if (in_array ($key, $this->_fixedschema)) {
 
-                    if (in_array($key, $this->_fixedschema))
-                    {
-
-                                        $rows = $this->_fixed->fetchAll(
+                        $rows = $this->_fixed->fetchAll (
                             $this->_fixed
-                                ->select()
-                                ->from(
-                                    $this->_fixed,
-                                        array($this->_type.'_id')
-                                       )
-                                ->where($key.' IN ('.implode(',',$values).')'));
+                                ->select ()
+                                ->from (
+                                $this->_fixed,
+                                array($this->_type . '_id')
+                            )
+                                ->where ($key . ' IN (' . implode (',', $values) . ')'));
 
 
-                        $ids = $rows->toArray();
+                        $ids = $rows->toArray ();
 
-                                        foreach ($ids as $id)
-                                        {
-                                                $id = $id[$this->_type.'_id'];
-                                                $this->_items[$id] = new H2D_Object($this->_type, $id);
-                                        }
+                        foreach ($ids as $id)
+                        {
+                            $id = $id[$this->_type . '_id'];
+                            $this->_items[$id] = new Evil_Object_H2D($this->_type, $id);
+                        }
                     }
                     else
                     {
-                        $rows = $this->_fluid->fetchAll(
+                        $rows = $this->_fluid->fetchAll (
                             $this->_fluid
-                                ->select()
-                                ->from(
-                                    $this->_fluid,
-                                        array('i')
-                                       )
-                                ->where('k = ?', $key)
-                                ->where('v IN ("'.implode(',',$values).'")'));
+                                ->select ()
+                                ->from (
+                                $this->_fluid,
+                                array('i')
+                            )
+                                ->where ('k = ?', $key)
+                                ->where ('v IN ("' . implode (',', $values) . '")'));
 
-                        $ids = $rows->toArray();
+                        $ids = $rows->toArray ();
 
-                                        foreach ($ids as $id)
-                                        {
-                                                $id = $id['i'];
-                                                $this->_items[$id] = new H2D_Object($this->_type, $id);
-                                        }
+                        foreach ($ids as $id)
+                        {
+                            $id = $id['i'];
+                            $this->_items[$id] = new Evil_Object_H2D($this->_type, $id);
+                        }
                     }
 
-                break;
+                    break;
             }
 
             return $this;
-       }
+        }
 
-       public function data()
-       {
+        public function data () {
             $output = array();
 
             foreach ($this->_items as $id => $item)
-                $output[$id] = $item->data();
+            $output[$id] = $item->data ();
 
             return $output;
-       }
+        }
 
-       public function addDNode ($key, $fn)
-       {
+        public function addDNode ($key, $fn) {
             foreach ($this->_items as $item)
-                $item->addDNode($key, $fn);
+            $item->addDNode ($key, $fn);
 
             return $this;
-       }
+        }
     }
