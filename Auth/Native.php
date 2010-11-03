8e6391be1a5371c557039ff0106450533b8055b2
artemy <artemy@513b48f0-a4ba-4d1f-9748-a1db15791537>
2010-11-03 10:59:41 +0000

    "+fix"
    git-svn-id: https://svn.ruimperium.com:444/repos/Common/Evil@2879 513b48f0-a4ba-4d1f-9748-a1db15791537

--- Auth/Native.php
+++ Auth/Native.php
@@ -8,31 +8,77 @@
  */
  
     class Evil_Auth_Native implements Evil_Auth_Interface 
-    {       
+    {   
+    	// Custom auth | Artemy
+    	private function _doCustomAuth($controller, $viewfile)
+    	{
+    		$login_view = new Zend_View();
+        	$login_view->setScriptPath(APPLICATION_PATH.dirname($viewfile));
+      
+        	// тут мы выдаем сообщения об ошибках
+        	// а не выкидываем эксепшны
+            if ($controller->getRequest()->isPost())
+            {
+                $data = $controller->getRequest()->getPost();
+                $user = Evil_Structure::getObject('user');	
+                $user->where('nickname','=', $data['username']);
+
+                if ($user->load())
+                {                       
+                        if ($user->getValue('password') == md5($data['password']))
+                        return $user->getId();
+                        else
+                        $login_view->error_message = 'Пароль указан не верно';
+                }
+                else $login_view->error_message = 'Пользователь не найден';
+                
+                $login_view->username = $login_view->escape($data['username']);
+            }       	  	
+
+        	$controller->view->form = $login_view->render(basename($viewfile));  	
+        	
+        	return -1;	
+    	}
+    	
+    	    
         public function doAuth ($controller)
         {
-            $form = new Evil_Auth_Form_Native();
-            $controller->view->form = $form;
-
-            if ($controller->getRequest()->isPost())
-                if ($form->isValid($_POST))
-                {
-                    $data = $form->getValues();
-
-                    $user = Evil_Structure::getObject('user');
-
-                    $user->where('nickname','=', $data['username']);
-
-                    if ($user->load())
-                    {                       
-                        if ($user->getValue('password') == md5($data['password']))
-                            return $user->getId();
-                        else
-                            throw new Evil_Exception('Password Incorrect', 4042);
-                    }
-                    else
-                        throw new Evil_Exception('Unknown user', 4044);
-                }
+        	// Support custom views for auth form
+        	$config = Zend_Registry::get('config');
+        	$config = (is_object($config)) ? $config->toArray() : $config;
+        	
+        	var_dump($config['evil']['auth']['native']['view']);
+        	
+        	if (isset($config['evil']['auth']['native']['view']) && !empty($config['evil']['auth']['native']['view']))
+        	{
+				return $this->_doCustomAuth($controller, $config['evil']['auth']['native']['view']);
+        	}
+        	else
+        	{
+        		$form = new Evil_Auth_Form_Native();           
+        		$controller->view->form = $form;
+        		
+	            if ($controller->getRequest()->isPost())
+	                if ($form->isValid($_POST))
+	                {
+	                    $data = $form->getValues();
+	
+	                    $user = Evil_Structure::getObject('user');
+	
+	                    $user->where('nickname','=', $data['username']);
+	
+	                    if ($user->load())
+	                    {                       
+	                        if ($user->getValue('password') == md5($data['password']))
+	                            return $user->getId();
+	                        else
+	                            throw new Evil_Exception('Password Incorrect', 4042);
+	                    }
+	                    else
+	                        throw new Evil_Exception('Unknown user', 4044);
+	                }        		
+        	}
+            
             return -1;
         }
 
