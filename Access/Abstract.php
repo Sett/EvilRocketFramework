ee5eef99e094e168a5aa55154958c2d0b49bf7d3
artemy <artemy@513b48f0-a4ba-4d1f-9748-a1db15791537>
2010-11-10 13:07:16 +0000

    ""
    git-svn-id: https://svn.ruimperium.com:444/repos/Common@2977 513b48f0-a4ba-4d1f-9748-a1db15791537

---
+++ Access/Abstract.php
@@ -0,0 +1,45 @@
+<?php
+    /**
+     * @author BreathLess, Artemy
+     * @name Evil_Access Plugin
+     * @type Zend Plugin
+     * @description: Abstract Access Engine for ZF
+     * @package Evil
+     * @subpackage Access
+     * @version 0.2
+     * @date 24.10.10
+     * @time 14:20
+     */
+
+    abstract class Evil_Access_Abstract extends Zend_Controller_Plugin_Abstract
+    {
+        protected static $_rules;
+
+        abstract public function _check ($subject, $controller, $action);
+        abstract public function init ();
+        abstract public function allowed($subject, $controller, $action);
+        abstract public function denied($subject, $controller, $action);
+          
+        public function routeShutdown(Zend_Controller_Request_Abstract $request)
+        {
+            parent::routeStartup ($request);
+            $this->init();
+           
+            if ($this->denied($request->getParam('id'),
+                $request->getControllerName(), $request->getActionName()))
+                    throw new Evil_Exception('Access Denied for '.$request->getControllerName().'::'.$request->getActionName(), 403);
+        }
+
+        protected function _resolve($condition, $object, $subject)
+        {
+            if ('*' !== $condition)
+                return self::$condition($object, $subject);
+            else
+                return true;
+        }
+        
+        protected function isOwner($subject)
+        {
+            return ($subject->owner() == Zend_Registry::get('userid'));
+        }
+    }
