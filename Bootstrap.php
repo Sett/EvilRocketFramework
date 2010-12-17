7f3562707a0938e88b31dc68be7a007d11f3c6c4
breathless <breathless@ruimperium.com>
2010-12-17 14:06:11 +0000

    New bootsrtap
    git-svn-id: https://svn.ruimperium.com:444/repos/Common/Evil@3976 513b48f0-a4ba-4d1f-9748-a1db15791537

---
+++ Bootstrap.php
@@ -0,0 +1,60 @@
+<?php
+
+    class Evil_Bootstrap extends Zend_Application_Bootstrap_Bootstrap
+    {
+        public function run ()
+        {
+
+           Zend_Registry::set('config', $config = parent::getOptions());
+
+           $front = $this->getResource('FrontController');
+           header('Content-type: text/html;charset=utf-8');
+
+           foreach ($config['bootstrap']['plugins'] as $plugin)
+                $front->registerPlugin(new $plugin);
+
+           Zend_Controller_Front::run($config['resources']['frontController']['controllerDirectory']);
+        }
+
+        protected function _initView()
+        {
+            Zend_Layout::startMvc(array(
+                'layoutPath' => APPLICATION_PATH.'/views/scripts/layouts/',
+                'layout'=> 'layouts/layout'
+            ));
+
+            $layout = Zend_Layout::getMvcInstance();
+
+            $view = $layout->getView();
+           $view->addHelperPath(APPLICATION_PATH.'/views/helpers');
+            $viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper(
+                'ViewRenderer'
+            );
+
+            $viewRenderer->setViewSuffix('phtml');
+            $viewRenderer->setView($view);
+
+            return $view;
+        }
+        /*
+        protected function _setRouter()
+        {
+           $router = new Score_Router();
+           // If router not a Zend_Controller_Router_Abstract, throw the Exception
+           if (!($router instanceof Zend_Controller_Router_Abstract))
+               throw new Exception('Incorrect config file: routes');
+           return $router;
+        }
+        */
+        protected function _initPlaceholders()
+            {
+                $this->bootstrap('View');
+
+                $view = $this->getResource('View');
+                $view->doctype('XHTML1_STRICT');
+                $view->headTitle('JunkieBytes');
+
+            }
+
+
+    }
