5a4c1421cc561b1e45ee130b897ed5511628e11e
sett <sett@513b48f0-a4ba-4d1f-9748-a1db15791537>
2010-12-23 13:24:26 +0000

    git-svn-id: https://svn.ruimperium.com:444/repos/Common/Evil@4189 513b48f0-a4ba-4d1f-9748-a1db15791537

--- Auth.php
+++ Auth.php
@@ -42,7 +42,7 @@
         {
             if (!isset($_SERVER['HTTP_USER_AGENT']))
                 $_SERVER['HTTP_USER_AGENT'] = '';
-            return sha1(mb_substr($_SERVER['HTTP_USER_AGENT'],0));
+            return sha1($_SERVER['HTTP_USER_AGENT']);
         }
 
         public function audit ()
