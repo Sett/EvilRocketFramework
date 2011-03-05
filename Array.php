786cb9129c4effca8a54dceb7657a63facc39998
nur <nur@513b48f0-a4ba-4d1f-9748-a1db15791537>
2011-03-05 11:11:27 +0000

    git-svn-id: https://svn.ruimperium.com:444/repos/Common@4870 513b48f0-a4ba-4d1f-9748-a1db15791537

---
+++ Array.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * 
+ * Посмотровщик массивов
+ * @author nur
+ *
+ */
+class Evil_Array
+{
+    /**
+     * 
+     * Доставалка из многомерных массивов
+     * удобно в случае использования ини конфигов
+     * @param string $path
+     * @param array $inputArray
+     * @example   
+     * $config = Zend_Registry::get('config');
+     * Evil_Array::get('file.upload.maxfilesize', $config);
+     */
+    public static function get ($path, array $inputArray,$default = null, $detelminer = '.')
+    {
+        $arrayOfPath = explode($detelminer, $path);
+        $value = $inputArray;
+        foreach ($arrayOfPath as $index)
+        {
+            if(is_array($value))
+            {
+                $value = $value[$index];
+            }
+             else 
+              return $default;
+        }
+        return $value;
+       
+    }
+}
