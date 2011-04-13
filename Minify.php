b793f0a0b64d967ef2d5f2db2d88768a0bf0583f
nur <nur@513b48f0-a4ba-4d1f-9748-a1db15791537>
2011-04-13 13:16:41 +0000

    git-svn-id: https://svn.ruimperium.com:444/repos/Common@6302 513b48f0-a4ba-4d1f-9748-a1db15791537

---
+++ Minify.php
@@ -0,0 +1,10 @@
+<?php
+$min_libPath = dirname(__FILE__) . '/Minify';
+set_include_path($min_libPath . PATH_SEPARATOR . get_include_path());
+include_once 'Minify/Minify.php';
+class Evil_Minify extends Minify
+{
+    
+}
+//Minify::setCache( APPLICATION_PATH . '/cache/', true);
+//Minify_Minify::setCache( APPLICATION_PATH . '/cache/', true);
