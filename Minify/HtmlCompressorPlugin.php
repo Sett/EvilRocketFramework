70cb8136b31f88f9fb33ac76d664ec148c72c4e3
nur <nur@513b48f0-a4ba-4d1f-9748-a1db15791537>
2011-04-14 06:56:50 +0000

    git-svn-id: https://svn.ruimperium.com:444/repos/Common/Evil@6319 513b48f0-a4ba-4d1f-9748-a1db15791537

--- Minify/HtmlCompressorPlugin.php
+++ Minify/HtmlCompressorPlugin.php
@@ -1,12 +1,12 @@
 <?php
-class Minify_HtmlCompressorPlugin extends Zend_Controller_Plugin_Abstract {
+class Evil_Minify_HtmlCompressorPlugin extends Zend_Controller_Plugin_Abstract {
     /**
      * Compress html code.
      *
      * @return void
      */
     public function dispatchLoopShutdown() {
-        $compressor = new Minify_HtmlCompressor();
+        $compressor = new Evil_Minify_HtmlCompressor();
         $response = $this->getResponse();
         $body = $response->getBody();
         $body = $compressor->filter($body);
