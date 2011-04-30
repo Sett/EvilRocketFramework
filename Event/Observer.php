090f25f0329da79363dcbe153437ed0478ea738b
Alexander M Artamonov <art.alex.m@gmail.com>
2011-05-01 00:00:54 +0400

    erc-6571: Событийная модель для сервисов. +__sleep() add in repo

---
+++ Event/Observer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Evil_Event_Observer
+ *
+ * Created by JetBrains PhpStorm.
+ * @author Alexander M Artamonov <art.alex.m@gmail.com>
+ * @package Evil
+ * @subpackage Evil Event
+ * @version 0.1
+ * @date 30.04.11
+ * @time 17:07
+ */
+ 
+class Evil_Event_Observer 
+{
+    
+}
