a2760efa19ff10609a27f1bed64f0b235c75114f
nur <nur.php@gmail.com>
2011-06-08 23:30:55 +0400

    Теперь по пацански еба

--- Bootstrap.php
+++ Bootstrap.php
@@ -8,14 +8,12 @@
            Zend_Registry::set('config', $this->_config = parent::getOptions());
 
            $front = $this->getResource('FrontController');
-       // $front->getResponse()->setHeader('Content-type','text/html;charset=utf-8');
            $front->setParam('bootstrap', $this);
-           header('Content-type: text/html;charset=utf-8');
            if (isset($this->_config['bootstrap']['plugins']))
                foreach ($this->_config['bootstrap']['plugins'] as $plugin)
                     $front->registerPlugin(new $plugin);
-
-           Zend_Controller_Front::run($this->_config['resources']['frontController']['controllerDirectory']);
+           $front->run($this->_config['resources']['frontController']['controllerDirectory']);
+          
         }
 
         protected function _initView()
@@ -35,11 +33,22 @@
 
             $viewRenderer->setViewSuffix('phtml');
             $viewRenderer->setView($view);
-
+            $view->setEncoding('UTF-8');        
+            $view->headMeta()->appendHttpEquiv('Content-Type', 'text/html;charset=utf-8');
             $config = parent::getOptions();
             $view->headTitle($config['system']['title']);
             $view->doctype('XHTML1_STRICT');
 
             return $view;
+        }
+        
+        
+         protected function _initModifiedFrontController ()
+        {
+            $this->bootstrap('FrontController');
+            $front = $this->getResource('FrontController');
+            $response = new Zend_Controller_Response_Http();
+            $response->setHeader('Content-Type', 'text/html; charset=UTF-8', true);
+            $front->setResponse($response);
         }
     }
