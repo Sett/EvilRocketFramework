22a58398cdae509f6598660dbb18f9947077b156
Alexander M Artamonov <art.alex.m@gmail.com>
2011-06-21 13:19:05 +0400

    ControllerCore. sceleton of entity

---
+++ Controller/Converter.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Evil_Controller_Converter
+ *
+ * Created by JetBrains PhpStorm.
+ * @author Alexander M Artamonov <art.alex.m@gmail.com>
+ * @type Utility
+ * @package Evil
+ * @subpackage Core
+ * @version 0.1
+ * @date 21.06.11
+ * @time 12:06
+ */
+ 
+class Evil_Controller_Converter
+{
+
+    protected $_entities = array();
+    protected $_defaultEntity = '';
+
+}
